e7b4a0cf582567c5ffda82fc7a4bf27e260b988f
CodeBreakerYT <gitagoswami456@gmail.com>
2026-03-13 09:18:42 +0530

    Use Case 4: Room Search & Availability Check

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,13 +1,101 @@
+<<<<<<< Updated upstream
 // UC1: Application Entry & Welcome Message
+=======
+// UC4: Room Search & Availability Check
+
+import java.util.HashMap;
+
+abstract class Room {
+    String roomType;
+    int beds;
+    int size;
+    double price;
+
+    Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    void displayRoom() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 350, 180);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 600, 350);
+    }
+}
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 10);
+        inventory.put("Double Room", 5);
+        inventory.put("Suite Room", 0);   // Example unavailable room
+    }
+
+    int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+}
+>>>>>>> Stashed changes
 
 public class HotelBookingApp {
 
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
         System.out.println("=================================");
         System.out.println("Welcome to Book My Stay");
         System.out.println("Hotel Booking System v1.0");
         System.out.println("=================================");
 
+=======
+        System.out.println("Book My Stay - Hotel Booking System v4.1");
+        System.out.println("---------------------------------------");
+
+        RoomInventory inventory = new RoomInventory();
+
+        Room[] rooms = {
+                new SingleRoom(),
+                new DoubleRoom(),
+                new SuiteRoom()
+        };
+
+        System.out.println("Available Rooms:");
+        System.out.println("----------------");
+
+        for (Room room : rooms) {
+
+            int available = inventory.getAvailability(room.roomType);
+
+            if (available > 0) {   // filter unavailable rooms
+                room.displayRoom();
+                System.out.println("Available: " + available);
+                System.out.println();
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
